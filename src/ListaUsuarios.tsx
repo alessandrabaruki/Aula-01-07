c621d8d06dae8ac4f921a1f0c26ce95e589bed12
alessandrabaruki <alembaruki03@gmail.com>
2024-07-14 10:51:50 -0400

    Initial commit

---
+++ src/ListaUsuarios.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ListaUsuarios from './ListaUsuarios';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <ListaUsuarios />
+    </div>
+  );
+};
+
+export default App;
